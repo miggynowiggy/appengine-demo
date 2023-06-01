06fe00a50abed5396f85ef5a0375c7184a8a8249
Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2023-06-02 01:31:39 +0800

    add something

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 
+def get_all(db: Session):
+    return db.query(models.User).all()
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
